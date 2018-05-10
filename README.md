9b202ad97b054f1882ef7a4787fcfb55757a5c23
Eric J <ejohnston092@gmail.com>
2018-05-09 23:52:43 -0400

    Added notes to code & a README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Psychic-Game
+The computer will pick a random letter. The user will have 9 attemps to guess the correct letter. If the user guesses correctly, they win, if they use up all 9 guesses, they lose.
+Guesses are recorded and displayed to the user.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,17 @@
 // Letters that computer can choose
 var lettersAZ = ["a", "b" ,"c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 // Initial variables
+var userGuess = null;
+var guessesMade = [];
+var guessesLeft = 9;
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-var guessesMade = [];
-var userGuess = null;
 // Makes computer choose a random letter
 var computerChoice = lettersAZ[Math.floor(Math.random() * lettersAZ.length)];
 console.log("Guess what letter i'm thinking of!");
 console.log("Computer picked: " + computerChoice)
 console.log("----------------------------------");
-
+//Keypress function
 document.onkeyup = function(event){
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     if (guessesMade.indexOf(userGuess) < 0 && lettersAZ.indexOf(userGuess) >= 0){
@@ -48,7 +48,7 @@ document.onkeyup = function(event){
         console.log("Computer picked: " + computerChoice);
         console.log("----------------------------------");
     };
-
+    // Shows the updated variables in html
     var html = "<p>Wins: " + wins + "<p>Losses: " + losses + "<p>Your guesses so far: " + guessesMade + "<p>Guesses left: " + guessesLeft;
     document.querySelector("#game").innerHTML = html;
 }
